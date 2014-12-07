614850d2a29dfa70ddd8904e46d74245083f4507
jeffreyxiong <jeffxiong017@gmail.com>
2014-12-06 20:50:06 -0600

    Javascript, Parallax, Design Revisions

--- scripts.js
+++ scripts.js
@@ -1,12 +1,27 @@
-// <!--  
+<!-- 
+// Document Ready Functions
 
-// function menu_click{
-	
-// }
+$(document).ready(function(){
+
+//	 PARALLAX SCROLL 	
+
+	// Cache window object
+	$window = $(window);
+	$('div[data-type="background"]').each(function(){
+		// Assign parallax object
+		var $bg = $(this);
+		// Custom scroll
+		$(window).scroll(function(){
+			// Scroll speed
+			var y = -($window.scrollTop() / $bg.data('speed'));
+			// Background position
+			var coords = '50%' + y + 'px'
+			// Move background
+			$bg.css({backgroundPosition: coords});
+		}); // end scroll
+	}); // end parallax
+
+});
 
 
-
-
-
-
-// -->
+-->
