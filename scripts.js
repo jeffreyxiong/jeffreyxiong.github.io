5e0a1f3d18d197f3da5d8bb4e001d67d1d60f4c4
jeffreyxiong <jeffxiong017@gmail.com>
2015-01-12 23:47:25 -0600

    Flexbox, Responsive + Mobile Styles

--- scripts.js
+++ scripts.js
@@ -1,27 +1,44 @@
 <!-- 
-// Document Ready Functions
+//// Document Ready Functions
 
 $(document).ready(function(){
 
-//	 PARALLAX SCROLL 	
+	//	PARALLAX SCROLL 	
 
-	// Cache window object
-	$window = $(window);
-	$('section[data-type="background"]').each(function(){
-		// Assign parallax object
-		var $bg = $(this);
-		// Custom scroll
-		$(window).scroll(function(){
-			// Scroll speed
-			var y = -($window.scrollTop() / $bg.data('speed'));
-			// Background position
-			var coords = '50%' + y + 'px'
-			// Move background
-			$bg.css({backgroundPosition: coords});
-		}); // end scroll
-	}); // end parallax
+		// Cache window object
+		$window = $(window);
+		$('section[data-type="background"]').each(function(){
+			// Assign parallax object
+			var $bg = $(this);
+			// Custom scroll
+			$(window).scroll(function(){
+				// Scroll speed
+				var y = -($window.scrollTop() / $bg.data('speed'));
+				// Background position
+				var coords = '50%' + y + 'px'
+				// Move background
+				$bg.css({backgroundPosition: coords});
+			}); // end scroll
+		}); // end parallax
+
+	// // HOVER EFFECTS
+
+	// 	// Love Box
+	// 	$(".love-img").hover(function(){
+	// 		$(this).css({"-webkit-filter": "grayscale(80%)"})
+	// 	}, function(){
+	// 		$(this).css({"-webkit-filter": "grayscale(0%)"})
+	// 	});
+	// 	$(".caption").hover(function(){
+	// 		$(".love-img").css({"-webkit-filter": "grayscale(80%)"})
+	// 	}, function(){
+	// 		$(".love-img").css({"-webkit-filter": "grayscale(0%)"})
+	// 	});
 
 });
+
+////	HOVER EFFECTS
+	
 
 
 -->
