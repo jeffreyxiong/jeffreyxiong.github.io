4411f5e52895e8f179c7089cca3c6b2e8673f4de
jeffreyxiong <jeffxiong017@gmail.com>
2015-02-05 01:26:49 -0600

    Work responsiveness

--- scripts.js
+++ scripts.js
@@ -2,6 +2,22 @@
 //// Document Ready Functions
 
 $(document).ready(function(){
+	//	SMOOTH SCROLL
+	//	between in-page anchors
+	$(function() {
+	    $('a[href*=#]:not([href=#])').click(function() {
+	    	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      		var target = $(this.hash);
+	      		target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      		if (target.length) {
+	        		$('html,body').animate({
+	          		scrollTop: target.offset().top
+	        		}, 1000);
+	        	return false;
+	      		}
+	    	}
+	  	});
+	});
 
 	//	PARALLAX SCROLL
 	//	media check (disable parallax on mobile devices)
