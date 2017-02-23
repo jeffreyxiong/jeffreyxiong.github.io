73c4c99fa27d858d8b935ac0f58da73473d6a0a6
jeffreyxiong <jeffxiong017@gmail.com>
2017-02-23 16:26:40 -0600

    photography page

--- js/grid.js
+++ js/grid.js
@@ -1,46 +1,114 @@
-var architecture = "img/gallery/architecture/"
-var nature = "img/gallery/nature/"
-var oddsEnds = "img/gallery/oddsends/"
-var people = "img/gallery/people/"
-
-$.ajax({
-    url : architecture,
-    success: function (data) {
-        $(data).find("a").attr("href", function (i, val) {
-            if( val.match(/\.(jpe?g)$/) ) { 
-                $(".grid").append( "<img src='"+ architecture + val +"' class='grid-item architecture'>" );
-            } 
-        });
-    }
-});
-
-
-
-
-
 $(function () {
 
-	// initialize grid
-	var $grid = $('.grid').isotope({
-		// options
-		itemSelector: '.grid-item',
-		layoutMode: 'masonry',
-		masonry: {
-			columnWidth: 400
+	var categoryUrl = ["img/gallery/nature/", "img/gallery/architecture/", "img/gallery/oddsends/", "img/gallery/people/"];
+	var categoryName = ["nature","architecture", "oddsends", "people"];
+	var categoryMap = {"nature": 0, "architecture": 1, "oddsends": 2, "people": 3};
+
+	var limit = 15;
+	var rise = limit;
+
+	var images = [];
+	var categorized = [[], [], [], []];
+
+	$.when(loadImageFromCategory(0), loadImageFromCategory(1), loadImageFromCategory(2), loadImageFromCategory(3)).done(function() {
+		shuffle(images);
+		for (var i = 0; i < limit; i++) {
+			$(".grid").append(images[i]);
 		}
 	});
 
+	// AJAX load image handler
+	function loadImageFromCategory(i) {
+		return $.ajax({
+			url: categoryUrl[i],
+			success: function(data) {
+				$(data).find("a").attr("href", function(j, val) {
+					// match jpg file type
+		            if( val.match(/\.(jpe?g)$/) ) {
+		            	// push html code to array
+		            	var html = "<div class='grid-item " + categoryName[i] + "' style='background: url(" + categoryUrl[i] + val + ") no-repeat center center; background-size: cover;'>";
+		            	images.push(html);
+						categorized[i].push(html);
+		            }
+				});
+			}
+		});
+	}
 
-	$grid.isotope({ filter: '.nature' });
-	$grid.isotope({ filter: '.people' });
-	$grid.isotope({ filter: '.places' });
-	$grid.isotope({ filter: '.misc' });
-	$grid.isotope({ filter: '*' });
+	// menu click handler
+	$(".filter-menu-item").click(function(event) {
+		// prevent default
+		event.preventDefault();
+		// stylize selected item
+		// reset all
+		$(".filter-menu-item").removeClass("filter-selected");
+		click_cond = [0, 0, 0, 0, 0]
 
+		// set current as clicked
+		var i = $(this).index();
+		click_cond[i] = click_cond[i] == 0 ? 1 : 0;
 
-	$('#menu-links').on('click', '.filter-a', function(){
-		var filterValue = $(this).attr('data-filter');
+		if (click_cond[i]) {
+			// add selected class
+			$(this).addClass("filter-selected");
+		}
+
+		// filter photos
+		// get filter value
+		var cat = $(this).attr("data-filter");
+		// load images
+		if (cat === "all") {
+			$("#load").removeClass("hidden");
+			// load from all
+			$(".grid-item").remove();
+			for (var i = 0; i < limit; i++) {
+				$(".grid").append(images[i]);
+			}
+		} else {
+			// load from specific category
+			// get index from filter value
+			$("#load").addClass("hidden");
+			var idx = categoryMap[cat]
+			// load proper image
+			$(".grid-item").remove();
+			for (var i = 0; i < categorized[idx].length; i++) {
+				$(".grid").append(categorized[idx][i]);
+			}
+		}
+
+		return false;
 	});
 
+	// load image handler
+	$("#load").click(function(event) {
+		// prevent default
+		event.preventDefault();
+		var cap = rise + limit;
+
+		if (rise + limit > images.length) {
+			cap = images.length
+		}
+
+		if (rise === images.length) {
+			return false;
+		}
+
+		for (var i = rise; i < cap; i++) {
+			$(".grid").append(images[i]);
+		}
+		rise += limit;
+
+		return false;
+	});
+
+	function shuffle(a) {
+	    var j, x, i;
+	    for (i = a.length; i; i--) {
+	        j = Math.floor(Math.random() * i);
+	        x = a[i - 1];
+	        a[i - 1] = a[j];
+	        a[j] = x;
+	    }
+	}
 });
 
--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,11 @@
 $(function() {
 
 	var menu_cond = 1;
+	var click_cond = [1, 0, 0, 0, 0];
 	var navMenu = Snap("#nav-icon-svg");
 
 	// fade in intro
-	$(".intro-box").fadeIn(2000);
+	$(".intro-box").fadeIn(2500);
 	// $(".intro-overlay").fadeIn(2000);
 
 	// menu click
@@ -17,8 +18,31 @@ $(function() {
 		navMenu.select("rect[width='25']").animate({width: "15"}, 500);
 
 		// toggle menu
-		$("nav").fadeTo(500, menu_cond);
+		if (menu_cond) {
+			$("nav").removeClass("hidden");
+			$("nav").fadeTo(500, menu_cond);
+		} else {
+			$("nav").fadeTo(500, menu_cond);
+			setTimeout(function(){ 
+				$("nav").addClass("hidden");
+			}, 500);
+		}
 
 		menu_cond = menu_cond == 0 ? 1 : 0;
 	});
+
+	$(window).scroll(function() {
+
+		if ($(window).scrollTop() > $("#header").outerHeight(true) + 30) {
+			$(".filter-menu-wrap").addClass("filter-fixed");
+			$(".filter-buffer").css({"display" : "block"});
+			// $("#header > h1").css({"font-size": "32px"}, 500);
+		} else {
+			$(".filter-menu-wrap").removeClass("filter-fixed");
+			$(".filter-buffer").css({"display" : "none"});
+			// $("#header > h1").css({"font-size": "28px"}, 500);
+		}
+	});
+
+
 });
